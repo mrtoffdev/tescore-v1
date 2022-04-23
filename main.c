fa83f23843f63aecb8304d7f0de84482b91e7ca3
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-24 06:18:07 +0800

    Initialized Masterlist Branch

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
-// main exec: Toff
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-
-#include "include/render/view/render.h"
 
 int main() {
 
-    refreshFrame();
-
-    system("pause");
     return 0;
 }
