1978788c2f8154db924fa3f9546959978f4cfd04
Toff <christopherjr.abadillos@bicol-u.edu.ph>
2022-04-23 22:21:11 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
-// main exec: Toff
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "include/io/io.h"
 
 int main() {
-    puts("Debugging: IO");
 
-    char* FileAddress = malloc(MAXSTRLEN);
-    printf("Enter Datasheet Name: (Default: demo.txt) ");
-    if(scanf("%s", FileAddress) != EOF){
-        testing(FileAddress);
-        free(FileAddress);
-    }
-
-    puts("Program Exit");
-    system("pause");
     return 0;
 }
