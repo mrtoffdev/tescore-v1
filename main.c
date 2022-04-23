50581ea98d4cd7484f4949fdcdb684dcc93731b8
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-24 04:49:57 +0800

    Added typeface references for render module

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
+// main exec: Toff
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#include "include/render/view/render.h"
+
+void red();
+void yellow();
+void reset();
 
 int main() {
+    printf("init\n");
+    char navIndex = 1;
+    while (navIndex != EOF){
+        navIndex = _getch();
+        switch (navIndex) {
+            case '1':
+                printf("focused panel 1\n");
+                renderFrame();
+                break;
+            case '2':
+                printf("focused panel 2\n");
+                break;
+            default:
+                break;
+        }
+    }
 
+
+//    red();
+//    printf("Hello ");
+//    yellow();
+//    printf("world\n");
+//    reset();
+
+    system("pause");
     return 0;
 }
+
+//void red () {
+//    printf("\033[1;31m");
+//}
+//
+//void yellow() {
+//        printf("\033[1;33m");
+//}
+//void reset () {
+//    printf("\033[0m");
+//}
