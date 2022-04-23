98884fe8af995ef3df761b0743a608006c771535
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-24 06:18:07 +0800

    Initialized Masterlist Branch

--- main.c
+++ main.c
@@ -1,29 +1,6 @@
-// main exec: Toff
 #include <stdio.h>
-#include <conio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include "include/lemao.h"
 
 int main() {
-    printf("Hello, World!\n");
-//    printf("Current typed text: %c", getchar());
-    char vard;
-    while (vard != EOF){
-        vard = _getch();
-        switch (vard) {
-            case '1':
-                printf("Typed 1\n");
-                break;
-            case '2':
-                printf("Typed 2\n");
-                break;
-            default:
-                printf("Invalid\n");
-                break;
-        }
-    }
 
-    system("pause");
     return 0;
 }
