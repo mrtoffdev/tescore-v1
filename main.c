311e4ccf56cf91f481f19f7a56d242c66371c49c
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-24 12:39:11 +0800

    Rendering Demo w/ ANSI escape sequences Testing console locale replacement to UTF8

--- main.c
+++ main.c
@@ -5,50 +5,10 @@
 
 #include "include/render/view/render.h"
 
-void red();
-void yellow();
-void reset();
-
 int main() {
-    puts("--- Renderer Debug ---\n");
 
-    int navIndex = 1;
-    while (navIndex != EOF){
-        navIndex = _getch();
-        switch (navIndex) {
-            case '1':
-                printf("focused panel 1\n");
-                refreshFrame();
-                break;
-            case '2':
-                printf("focused panel 2\n");
-                break;
-            case 'e':
-                puts("Testing E char on int navigation");
-                break;
-            default:
-                break;
-        }
-    }
-
-
-//    red();
-//    printf("Hello ");
-//    yellow();
-//    printf("world\n");
-//    reset();
+    refreshFrame();
 
     system("pause");
     return 0;
 }
-
-//void red () {
-//    printf("\033[1;31m");
-//}
-//
-//void yellow() {
-//        printf("\033[1;33m");
-//}
-//void reset () {
-//    printf("\033[0m");
-//}
