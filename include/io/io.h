3e203c24cd8a3887caa5ccc3bb9d929a053142f0
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-26 06:31:00 +0800

    Updated IO interface for other modules

--- include/io/io.h
+++ include/io/io.h
@@ -1,8 +1,16 @@
-//
-// Created by Administrator on 4/25/2022.
-//
-
 #ifndef TESCORE_IO_H
 #define TESCORE_IO_H
 
-#endif //TESCORE_IO_H
+#include "../model/datasheet.h"
+
+#define FILEADDRESS "../demo.txt"
+#define MAXSTRLEN 255
+
+void testing();
+
+void apIndex(INDEX);
+void delIndex(INDEX);
+
+void initDatasheet();
+
+#endif
