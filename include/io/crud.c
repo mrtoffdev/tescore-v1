1ac302eec2cc15d6a02d954ddfeaca5e3cb5d6c2
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-04-26 06:31:53 +0800

    Testing FileOps through header file

--- include/io/crud.c
+++ include/io/crud.c
@@ -1,3 +1,24 @@
-//
-// Created by Administrator on 4/25/2022.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "io.h"
+
+void testing(){
+    FILE *DIB;
+    if ((DIB = fopen(FILEADDRESS,"r")) == NULL){
+        printf("Invalid Filename\n");
+        system("pause");
+        exit(1);
+    }
+
+    char *temp = malloc(MAXSTRLEN);
+    fscanf(DIB, "%s", temp);
+
+    printf("%s\n", temp);
+}
+
+void append(){
+
+
+}
+
