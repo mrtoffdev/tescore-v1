6b849f62c6f2e0aac3c62e82271e50745748d167
CasualCodes <jovicfrancisbulawan.rayco@bicol-u.edu.ph>
2022-04-27 15:34:13 +0700

    added a LOT of comments

--- include/sort/ranker/module.c
+++ include/sort/ranker/module.c
@@ -13,15 +13,22 @@ typedef struct index
 
 typedef struct subsheet
 {
-    index unsortedList[100]; // This is the only problem tbh
+    index unsortedList[100]; 
+    /* NOTE: This is the only problem tbh if we really want dynamic sizes. 
+    *  I also cannot use macro definitions here for some reason.
+    */
 } subsheet;
 
 void bubbleSort(char string_array[][LIMIT], int score_array[], int lines);
-subsheet rankSorterMkII(subsheet subsheetvar);
+subsheet ranker(subsheet subsheetvar);
 
 int main(void)
 {
-    // Placeholder for arguments
+    // Sample subsheet
+    /* NOTE:The initialization had two (or three) sets of curly braces since this is the least
+    *  confusing/error prone way for me to say 'samplesubsheet.unsortedlist = {items}'. Using the latter
+    *  leads to errors for some reason
+    */
     subsheet sampleSubsheet = 
     {
         {   // unsortedList
@@ -39,9 +46,10 @@ int main(void)
         }
     };
     
-    subsheet sortedSubsheet = rankSorterMkII(sampleSubsheet);
+    // Passing the subsheet as an argument to the function and returning as a sorted subsheet
+    subsheet sortedSubsheet = ranker(sampleSubsheet);
 
-    // // List is sorted at this point //
+    // List is sorted at this point. Just printing the sorted subsheet //
     for (int i = 0; i<10; i++)
     {
         printf("%-30s\t%d\n", sortedSubsheet.unsortedList[i].studentName, sortedSubsheet.unsortedList[i].studentScore);
@@ -50,18 +58,21 @@ int main(void)
     return 0;
 }
 
-subsheet rankSorterMkII(subsheet subsheetvar)
+// NOTE: The argument here is passed by value. Do inform me if you want this to pass by reference.
+subsheet ranker(subsheet subsheetvar)
 {
     subsheet newSubsheet;
     int lines, i, size;
+    
+    // Finding the size of the subsheetvar.unsortedList[i] since the sizeof way did not work
     for (int i = 0; subsheetvar.unsortedList[i].studentName != NULL; i++)
     {
-        size++; // the sizeof solution did not work for some reason but yea
+        size++;
     }
 
+    // Getting the values/kinda 'disassembling' the subsheet.
     int score_array[SIZE] = {0};
     char name_parray[SIZE][LIMIT];
-
     for (i = 0; i<size; i++)
     {
         score_array[i] = subsheetvar.unsortedList[i].studentScore;
@@ -69,8 +80,10 @@ subsheet rankSorterMkII(subsheet subsheetvar)
         lines++;
     }
 
-    bubbleSort(name_parray, score_array, size); // this has something weird but yea
+    // NOTE: I once tried placing my bubblesort code in this function, but it lead to errors for some reason.
+    bubbleSort(name_parray, score_array, size);
 
+    // Placing the sorted values to the new subsheet
     for (int i = 0; i<size; i++)
     {
         newSubsheet.unsortedList[i].studentScore = score_array[i];
@@ -80,17 +93,18 @@ subsheet rankSorterMkII(subsheet subsheetvar)
     return newSubsheet;
 }
 
-// // needs amount of lines
+// Standard bubbleSort code
 void bubbleSort(char string_array[][LIMIT], int score_array[], int lines)
 {
-    int pass, size, i, intswap;
+    int pass, i, intswap;
     char stringswap[LIMIT];
 
     for(pass = 1; pass < lines; pass++)
     {
         for(i = 0; i < lines-pass; i++)
         {
-            if(score_array[i] < score_array[i+1])
+            // Sorts based on grades
+            if(score_array[i] < score_array[i+1]) 
             {
                 intswap = score_array[i];
                 score_array[i] = score_array[i+1];
@@ -100,6 +114,7 @@ void bubbleSort(char string_array[][LIMIT], int score_array[], int lines)
                 strcpy(string_array[i], string_array[i+1]);
                 strcpy(string_array[i+1], stringswap);
             }
+            // Else if grades are equal, sort alphabetically
             else if ((score_array[i] == score_array[i+1]) && (strcmp(string_array[i], string_array[i+1]) > 0))
             {
                 strcpy(stringswap, string_array[i]);
