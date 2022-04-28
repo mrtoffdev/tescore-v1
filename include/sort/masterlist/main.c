ca4d23a4d81439e38f77b787b98de0ec352141a8
Dart Evader <somewhere@overrainbow.com>
2022-04-28 15:10:13 +0800

    alternative module skeleton w/ tests

--- include/sort/masterlist/main.c
+++ include/sort/masterlist/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "alphabeticallySort.h"
+#include "alphaMergeSort.h"
 
 #define MAX 999
 
@@ -27,6 +28,28 @@ int main() {
     else printf("x");
     
     // test 3
+    if (strcmp(studentList[1].name, "Nylon Jeepney") == 0) printf("*");
+    else printf("x");
+    
+    // reshuffle for merge sort version
+    strcpy(studentList[0].name, "Zebra Helicopter");
+    studentList[0].score = 83;
+    strcpy(studentList[1].name, "Apple Bus"); 
+    studentList[1].score = 95;
+    strcpy(studentList[2].name, "Nylon Jeepney"); 
+    studentList[2].score = 91;
+    
+    alphaMergeSort(studentList, 0, 3);
+    
+    // test 4
+    if (strcmp(studentList[0].name, "Apple Bus") == 0) printf("*");
+    else printf("x");
+    
+    // test 5
+    if (strcmp(studentList[2].name, "Zebra Helicopter") == 0) printf("*");
+    else printf("x");
+    
+    // test 6
     if (strcmp(studentList[1].name, "Nylon Jeepney") == 0) printf("*");
     else printf("x");
     
