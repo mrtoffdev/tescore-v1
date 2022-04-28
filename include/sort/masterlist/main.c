0ab6bf2d17f09997fe89c7fbd7b8dbbeaa49306f
Dart Evader <somewhere@overrainbow.com>
2022-04-28 14:14:02 +0800

    switched from 2D char* array to struct array

--- include/sort/masterlist/main.c
+++ include/sort/masterlist/main.c
@@ -8,24 +8,26 @@
 int main() {
     
     // sample student list
-    char *studentList[3][MAX] = {
-        {"Bu Yaoze", "87"},
-        {"Fei Wazai", "90"},
-        {"Aine Hakum", "93"}
-    };
+    struct strec studentList[3];
+    strcpy(studentList[0].name, "Zebra Helicopter");
+    studentList[0].score = 83;
+    strcpy(studentList[1].name, "Apple Bus"); 
+    studentList[1].score = 95;
+    strcpy(studentList[2].name, "Nylon Jeepney"); 
+    studentList[2].score = 91;
     
     alphabeticallySort(studentList, 3);
     
     // test 1
-    if (strcmp(studentList[0][0], "Aine Hakum") == 0) printf("*");
+    if (strcmp(studentList[0].name, "Apple Bus") == 0) printf("*");
     else printf("x");
     
     // test 2
-    if (strcmp(studentList[2][0], "Fei Wazai") == 0) printf("*");
+    if (strcmp(studentList[2].name, "Zebra Helicopter") == 0) printf("*");
     else printf("x");
     
     // test 3
-    if (strcmp(studentList[1][0], "Bu Yaoze") == 0) printf("*");
+    if (strcmp(studentList[1].name, "Nylon Jeepney") == 0) printf("*");
     else printf("x");
     
     // end of tests
