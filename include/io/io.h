b3f0700db7f9d4ea08cdad29a67eb795def5f3a1
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-05 04:54:54 +0800

    Updated fetch data & io header

--- include/io/io.h
+++ include/io/io.h
@@ -10,14 +10,26 @@
 #define DEFAULTFILEADDRESS "../demo.txt"
 #define MAXSTRLEN 255
 
+/*========================================
+                  Testing
+  ========================================*/
 void testing(char*);
-void fetchData(FILE*);
+void fetchData(FILE*, char*, char*, int);
 void closeFile(FILE*);
 
+/*========================================
+                    CRUD
+  ========================================*/
 void appendIndex(INDEX);
 void delIndex(INDEX);
 
 void initSheet();
 void accessDatasheet();
 
+/*========================================
+                    SORT
+  ========================================*/
+
+SUBSHEET rankerModule();
+SUBSHEET masterlistModule();
 #endif
