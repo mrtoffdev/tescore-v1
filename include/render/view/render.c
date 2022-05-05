068d0fbd078e564825567b5d94c484fa1b9ce692
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-05 17:17:36 +0800

    Added Rendering System plan for merge

--- include/render/view/render.c
+++ include/render/view/render.c
@@ -1,9 +1,36 @@
+
+/*========================================
+      FRAME RENDERING
+      Author: Christopher Abadillos Jr.
+  ========================================*/
+
+/* RENDERING SYSTEM
+
+    FUNCTION CALLS
+        - refreshFrame()    : Renders the main interactive GUI for the first time
+        - terminatePrompt() : Renders the program termination prompt
+
+    BAR GRAPH - 2D Char Matrix System
+        - Divide the number of students into 10
+            - If divisible by 10, set all notches with $Quotient increments
+            - If not divisible by 10, divide by 9, set all notches with $Quotient increments, and set the last / highest notch to remainder
+                - e.g. 81 =
+
+    REFRESHING FRAMES
+        - Refresh call will re-render all three panels
+        - refreshFrame() will also be the primary call when rendering the panels for the first time
+
+*/
+
 #include <stdio.h>
 #include <conio.h>
 
 #include "render.h"
 
 #define MAX_PASS_CHAR 30
+
+// GLOBAL VARS
+short panelID = 1;
 
 void refreshFrame(){
 
@@ -15,7 +42,7 @@ void refreshFrame(){
 
     printf("----  Renderer Debug: ----\n");
     printf("-----------------------------------\n"
-           "|                                 |\n");
+                  "|                                 |\n");
     printf("-----------------------------------");
     printf("\x1b[1A");
     printf("\x1b[33D");
@@ -40,6 +67,16 @@ void refreshFrame(){
     }
 
 }
+
+void terminatePrompt(){
+
+}
+
 // Notes:
 // #include <windows.h>
 // SetConsoleOutputCP(CP_UTF8);
+
+/* ANSI REFERENCES
+
+
+ */
