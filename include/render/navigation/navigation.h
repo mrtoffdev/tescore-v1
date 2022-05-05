c882d84e0856b3892742b9f26f8eef2acd2d11b7
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-05 17:20:06 +0800

    Updated Rendering system reference Updated method prototypes

--- include/render/navigation/navigation.h
+++ include/render/navigation/navigation.h
@@ -1,7 +1,18 @@
 #ifndef TESCORE_NAVIGATION_H
 #define TESCORE_NAVIGATION_H
 
-void navigate(int);
-void resetNav();
+/* RENDERING SYSTEM
 
+    FUNCTION CALLS
+        VOID
+        - parseNavigationKey(key, panelID)      : Checks passed in key & performs corresponding action
+
+        RETURN
+        - <short> fetchPanelID()                : Returns the panelID (short)
+
+*/
+
+// PUBLIC FUNCTIONS
+void parseNavigationKey(char key, short panelID);
+short fetchPanelID();
 #endif
