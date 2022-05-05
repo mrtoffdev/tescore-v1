c9d1e362fc94b0c12925226189561eec0364e635
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-05 17:16:32 +0800

    Created rankerModule interface

---
+++ include/sort/ranker/rankerModule.h
@@ -0,0 +1,13 @@
+#ifndef TESCORE_RANKERMODULE_H
+#define TESCORE_RANKERMODULE_H
+
+#include "../../model/datamodel.h"
+
+// Module Parameters
+#define SIZE 100
+#define LIMIT 509 // Standard limit for char ANSI Compatibility
+
+// Method Calls
+SUBSHEET ranker(SUBSHEET unsortedSubsheet);
+
+#endif
