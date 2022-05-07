38e85a5eb28a2473c83af01e7c482b73d1570193
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-07 23:12:55 +0800

    Moved reference model

--- include/model/datamodel.h
+++ include/model/datamodel.h
@@ -1,7 +1,7 @@
 /*========================================
-     Primary interface for unit tests
+      DATA MODELS INTERFACE
+      Author: Christopher Abadillos Jr.
   ========================================*/
-
 #ifndef TESCORE_DATAMODEL_H
 #define TESCORE_DATAMODEL_H
 
@@ -10,3 +10,25 @@
 #include "../model/subsheet.h"
 
 #endif
+
+/*  Reference Model
+    Datasheet
+       |
+       |--- Sheet Name
+       |--- Sheet Author
+       |--- RankedSheet/
+       |     |
+       |     |--- Index Collection/
+       |           |
+       |           |--- Index 1 (Student 1)
+       |           |--- Index 2 (Student 2)
+       |           |--- Etc...
+       |
+       |--- MasterSheet/
+             |
+             |--- Index Collection
+                   |
+                   |--- Index 1 (Student 1)
+                   |--- Index 2 (Student 2)
+                   |--- Etc...
+*/
