a49e1a1897d99ae6eb935d684215b25c09f6a72d
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-07 23:13:50 +0800

    Added docs & author

--- include/render/navigation/navigation.c
+++ include/render/navigation/navigation.c
@@ -1,3 +1,7 @@
+/*========================================
+      NAVIGATION SYSTEM
+      Author: Christopher Abadillos Jr.
+  ========================================*/
 #include <stdio.h>
 
 #include "navigation.h"
@@ -5,18 +9,35 @@
 
 /* NAVIGATION SYSTEM
 
+    GLOBAL VARIABLES
+    - cellIndex         position of navigation cursor in table, from top to bottom
+    - cellSelection     position of navigation cursor within cell
+        - <A>           left (L)
+        - <D>           right (R)
+
     GLOBAL NAVIGATION
-    - <1><2><3>     switching between panels
-    - <W><A><S><D>  panel / table navigation
-    - <Q>           program termination prompt
+    - <1><2><3>         switching between panels
+    - <W><A><S><D>      panel / table navigation
+    - <Q>               program termination prompt
 
     MASTERLIST NAVIGATION
-    - <E>           editing highlighted cell
-    - <R>           removing highlighted cell
+    - <E>               editing highlighted cell
+    - <R>               removing highlighted cell
 
     RANKER LIST NAVIGATION
-    - <E>           expand ranking list
-    - <Q>           close expanded tab
+    - <E>               expand ranking list
+    - <Q>               close expanded tab
+
+    OPERATION MODES
+    - 1                 view / read only
+    - 2                 edit permissions
+    - 3                 write permissions
+    - 4                 delete permissions
+
+    CELL SELECTION POSITIONS
+    - X                 default (all highlighted, no edits can be made)
+    - L                 editing index name
+    - R                 editing index value
 
 */
 
