39b2e0ccb1f50eb77bbc8bba7e356719f9b5229e
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-07 23:15:41 +0800

    Added function call sections for interface

--- include/render/view/render.c
+++ include/render/view/render.c
@@ -70,9 +70,91 @@ void terminatePrompt(){
 
 }
 
-// Notes:
-// #include <windows.h>
-// SetConsoleOutputCP(CP_UTF8);
+// =========== UI ELEMENTS ===========
+void clearScreen(){
+    printf("\x1b[2J\x1b[H");
+}
+
+void renderWhiteSpace(int spaceSize){
+    for (int i = 0; i < spaceSize; ++i) {
+        printf("\n");
+    }
+}
+
+void renderSeparator(){
+    puts("\t-------------------------------------------------------------------------------------------------------------");
+
+}
+
+void indentCursor(){
+    printf("\t");
+}
+
+
+void renderHeader(int sessionStudentCount){
+    printf("\t|\t\t\t- Data Sheet -\t\t\t    |\t - Top 10 Students / %4d - \t|   Score   |\n",
+           sessionStudentCount);
+}
+
+void renderSubHeader(int gradeScaling[11], char *sheetName){
+    int bottom[11] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    printf("\t|    |%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d| Sheet Name: %-18s \t|\t    |\n",
+           gradeScaling[0],
+           gradeScaling[1],
+           gradeScaling[2],
+           gradeScaling[3],
+           gradeScaling[4],
+           gradeScaling[5],
+           gradeScaling[6],
+           gradeScaling[7],
+           gradeScaling[8],
+           gradeScaling[9],
+           gradeScaling[10],
+           sheetName);
+}
+
+void renderUpperRow(int studentScaling[], int gradeScaling[], char matrixGraph[][11], int rowScale){
+
+    int tableValY[] = {10};
+    int bottom[] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    char *barMatrix[10][10];
+
+    for (int i = 0; i < rowScale; ++i) {
+        printf("\t|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|%4d|  • Barack Obama \t\t\t|    100    |\n",
+               tableValY[0],
+               bottom[0],
+               bottom[1],
+               bottom[2],
+               bottom[3],
+               bottom[4],
+               bottom[5],
+               bottom[6],
+               bottom[7],
+               bottom[8],
+               bottom[9],
+               bottom[10]);
+    }
+
+}
+
+
+void renderMasterListHeader(){
+    printf("\t|\t\t\t\t- Student Submission List -\t\t\t\t\t|           |\n");
+}
+
+void renderMasterListRow(){
+    printf("\t| • %-90s  |    %4d   |\n", "Michael Reeves", 23);
+}
+
+// =========== TESTS ===========
+
+/* ENABLING UTF8 SUPPORT FOR MICROSOFT WINDOWS CONSOLES
+
+    #include <windows.h>
+    SetConsoleOutputCP(CP_UTF8);
+
+*/
 
 /* ANSI REFERENCES
 
