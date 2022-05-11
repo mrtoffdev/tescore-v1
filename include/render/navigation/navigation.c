1bad86b53cca9f3b84d3edd15b7dbfdec2ea08f8
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-12 05:45:54 +0800

    Isolated _getchar() returnless buffer within navigation.c

--- include/render/navigation/navigation.c
+++ include/render/navigation/navigation.c
@@ -3,6 +3,8 @@
       Author: Christopher Abadillos Jr.
   ========================================*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
 #include "navigation.h"
 #include "../view/render.h"
@@ -10,7 +12,7 @@
 /* NAVIGATION SYSTEM
 
     GLOBAL VARIABLES
-    - cellIndex         position of navigation cursor in table, from top to bottom
+    - renderCellIndex         position of navigation cursor in table, from top to bottom
     - cellSelection     position of navigation cursor within cell
         - <A>           left (L)
         - <D>           right (R)
@@ -42,103 +44,109 @@
 */
 
 //#region PRIVATE FUNC PROTS
-void safeRemoveCell(char*);
-void safeEditCell(char, char*);
-void switchNavPanel(short, char*);
+void safeRemoveCell(char* []);
+void safeEditCell(char, char* []);
+void switchNavPanel(short, char* []);
 //#endregion
 
 //#region GLOBAL VARIABLES
-short   cellIndex,
+short   renderCellIndex,
+        globalCellIndex,
         globalPanelID,
         operationMode = 1,
         debugMode = 0;
+int     NAVKEY = '1';
 char    cellSelection = 'X';
+char**  localCommandLog;
 //#endregion
 
 // OPERATIONS
-void navigationKeyHandler(DATASHEET sessionSheet,int key, int maxIndexes, char* commandLog){
+void navigationKeyHandler(DATASHEET sessionSheet, int maxIndexes, char* commandLog[]){
     // SAVE CURRENT PANELID
     short prevPanelID = globalPanelID;
 
-    switch (key) {
+    while (NAVKEY != EOF){
+        NAVKEY = _getch();
 
-        //#region OPERATION KEYS
-        case 'Q':
-        case 'q':
-            /*
-                Q OPERATION
-                    - Checks if the cell selection is not default & currently in edit mode
-                        - If true, gets out of edit mode & resets cursor position
-                        - If not true (everything is on default i.e. at home screen
-                            - Show terminate program prompt
-            */
-            if((cellSelection != 'X') && (operationMode == 2)){
-                // reset global navigation values
-                commandLog = "Resetted all global nav values\n";
-                cellSelection = 'X';
-                operationMode = 1;
-                break;
-            } else {
-                terminatePrompt();
-                break;
-            }
+        switch (NAVKEY) {
 
-        case 'E':
-        case 'e':
-            /*
-                E OPERATION
-                    - Checks if the global nav variables are at default
-                        - If true, starts edit mode & repositions cursor to left value
-                        - If false, starts write mode for the selected cell position
+            //#region OPERATION KEYS
+            case 'Q':
+            case 'q':
+                /*
+                    Q OPERATION
+                        - Checks if the cell selection is not default & currently in edit mode
+                            - If true, gets out of edit mode & resets cursor position
+                            - If not true (everything is on default i.e. at home screen
+                                - Show terminate program prompt
+                */
+                if((cellSelection != 'X') && (operationMode == 2)){
+                    // reset global navigation values
+                    commandLog[0] = "Resetted all global nav values\n";
+                    cellSelection = 'X';
+                    operationMode = 1;
+                    break;
+                } else {
+                    terminatePrompt();
+                    break;
+                }
 
-                MODES
-                    - Edit Mode
-                        - Highlights either the index name or the index value
+            case 'E':
+            case 'e':
+                /*
+                    E OPERATION
+                        - Checks if the global nav variables are at default
+                            - If true, starts edit mode & repositions cursor to left value
+                            - If false, starts write mode for the selected cell position
 
-                    - Write Mode
-                        - The highlighted element can now be changed
-            */
-            if((operationMode == 1) && (cellSelection == 'X')){
-                commandLog = "Operation Mode: 2";
-                operationMode = 2;
-                cellSelection = 'L';
-                break;
-            } else
-            if((operationMode == 2) && (cellSelection != 'X')){
-                commandLog = "Operation Mode: 3";
-                operationMode = 3;
-                safeEditCell(cellSelection, commandLog);
-                break;
-            } else break;
+                    MODES
+                        - Edit Mode
+                            - Highlights either the index name or the index value
 
-        case 'R':
-        case 'r':
-            if((operationMode == 1) && (cellSelection == 'X')){
-                safeRemoveCell(commandLog);
-                break;
-            } else break;
-            //#endregion
+                        - Write Mode
+                            - The highlighted element can now be changed
+                */
+                if((operationMode == 1) && (cellSelection == 'X')){
+                    commandLog[0] = "Operation Mode: 2";
+                    operationMode = 2;
+                    cellSelection = 'L';
+                    break;
+                } else
+                if((operationMode == 2) && (cellSelection != 'X')){
+                    commandLog[0] = "Operation Mode: 3";
+                    operationMode = 3;
+                    safeEditCell(cellSelection, commandLog);
+                    break;
+                } else break;
 
-        //#region NAVIGATION KEYS
+            case 'R':
+            case 'r':
+                if((operationMode == 1) && (cellSelection == 'X')){
+                    safeRemoveCell(commandLog);
+                    break;
+                } else break;
+                //#endregion
+
+            //#region NAVIGATION KEYS
         case 'W':
         case 'w':
-            if(cellIndex > 0) {
-                cellIndex--;
+            if(renderCellIndex > 0) {
+                renderCellIndex--;
 
                 // Testing
-                commandLog = "Decremented cellIndex:";
+                commandLog[0] = "Decremented renderCellIndex:";
 
             }
             break;
 
         case 'S':
         case 's':
-            if(cellIndex < maxIndexes) {
-                cellIndex++;
+            if(renderCellIndex < maxIndexes) {
+                renderCellIndex++;
 
                 // Testing
-                commandLog = "Incremented cellIndex:";
-                printf("Incremented cellIndex: %d ", cellIndex);
+                commandLog[0] = "Incremented renderCellIndex:";
+                printf("Incremented renderCellIndex: %d ", renderCellIndex);
             }
             break;
 
@@ -157,55 +165,65 @@ void navigationKeyHandler(DATASHEET sessionSheet,int key, int maxIndexes, char* 
                 printf("\tCell Selection: %c ", cellSelection);
                 break;
             } else break;
-        //#endregion
+            //#endregion
 
-        //#region PANEL KEYS
+            //#region PANEL KEYS
         case '1':
             globalPanelID = 1;
+            commandLog[0] = "Rendering Panel No: 1";
             break;
 
         case '2':
             globalPanelID = 2;
+            commandLog[0] = "Rendering Panel No: 2";
             break;
 
         case '3':
             globalPanelID = 3;
+            commandLog[0] = "Rendering Panel No: 3";
             break;
 
         default:
             break;
-        //#endregion
+            //#endregion
 
+        }
+
+        indentCursor(1);
+
+        if(renderCellIndex){
+
+        }
+
+        printf("Cell Selection: %c ", cellSelection);
+        printf("Operation Mode: %d \n", operationMode);
+
+        // check if panelID values have changed and render frame ID
+        // prevents re-rendering the frame if panelID hasn't changed
+        if (globalPanelID != prevPanelID){
+
+            switchNavPanel(globalPanelID, commandLog);
+        }
+
+        refreshFrame(sessionSheet, commandLog, globalPanelID);
     }
-
-    printf("Cell Selection: %c ", cellSelection);
-    printf("Operation Mode: %d \n", operationMode);
-
-    // check if panelID values have changed and render frame ID
-    // prevents re-rendering the frame if panelID hasn't changed
-    if (globalPanelID != prevPanelID){
-
-        switchNavPanel(globalPanelID, commandLog);
-    }
-
-    refreshFrame(sessionSheet, commandLog);
 }
 
-void safeEditCell(char selection, char* commandLog){
+void safeEditCell(char selection, char* commandLog[]){
     char prevCellSelection = cellSelection;
     puts("Editing Cell:");
 
     switch (cellSelection) {
         case 'L':
-            commandLog = "Editing Left of Cell";
+            commandLog[0] = "Editing Left of Cell";
             break;
 
         case 'R':
-            commandLog = "Editing Right of Cell";
+            commandLog[0] = "Editing Right of Cell";
             break;
 
         default:
-            commandLog = "Not Editing a Cell Right Now";
+            commandLog[0] = "Not Editing a Cell Right Now";
             break;
     }
 
@@ -213,13 +231,16 @@ void safeEditCell(char selection, char* commandLog){
     operationMode = 2;
 }
 
-void safeRemoveCell(char* commandLog){
-    commandLog = "Removing Cell";
+void safeRemoveCell(char* commandLog[]){
+    commandLog[0] = "Removing Cell";
 }
 
-void switchNavPanel(short id, char* commandLog){
-    commandLog = "Rendering Panel No: ";
+void switchNavPanel(short id, char* commandLog[]){
     printf("%d", id);
+}
+
+void appendCommandLogEntry(commandLog){
+
 }
 
 // RETURNS
