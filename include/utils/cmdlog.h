e526ff664b2794db66fe0e23f200f58a3c2ce1f2
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-12 05:46:14 +0800

    Initialized development of commandLog system

---
+++ include/utils/cmdlog.h
@@ -0,0 +1,10 @@
+/*========================================
+      COMMAND LOG SYSTEM
+      Author: Christopher Abadillos Jr.
+  ========================================*/
+#ifndef TESCORE_CMDLOG_H
+#define TESCORE_CMDLOG_H
+
+
+
+#endif //TESCORE_CMDLOG_H
