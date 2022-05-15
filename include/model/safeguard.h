de9f3b203eb8ac7e0c1a7591e1405369c912beb1
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-15 10:43:10 +0800

    Created Safeguard context (Unused)

---
+++ include/model/safeguard.h
@@ -0,0 +1,17 @@
+#ifndef TESCORE_SAFEGUARD_H
+#define TESCORE_SAFEGUARD_H
+
+#include "../io/lib/aes.h"
+
+typedef struct Safeguardctx {
+    struct      AES_ctx ctx;
+    uint8_t*    encrypted_dataBuffer,
+                decrypted_dataBuffer,
+                passphrase,
+                saltIV;
+    uint8_t     bufferSize;
+    uint64_t    padding;
+    char*       plain_strSrcBuffer;
+} Safeguardctx;
+
+#endif //TESCORE_SAFEGUARD_H
