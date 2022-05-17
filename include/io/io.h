90a0300857fa197c7181082183e15f6684be59d1
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-17 19:01:17 +0800

    Removed legacy func prototypes

--- include/io/io.h
+++ include/io/io.h
@@ -5,49 +5,29 @@
 #ifndef TESCORE_IO_H
 #define TESCORE_IO_H
 
-// Models
 #include "../model/datamodel.h"     // Needed for Datasheet context
 #include "../model/renderctx.h"     // Needed for Renderctx context
 #include "../io/lib/aes.h"
 
-// Interface Parameters
 #define MAXADDRLENGTH 509                   // Standard string char limit for ANSI Compatibility
 #define DEFAULTFILEADDRESS "../demo.txt"    // Default demo.txt outside ../cmake-build-debug
 
-
-/*========================================
-                  Testing
-  ========================================*/
+// Scripts
 void script_fileopsTesting();
 void fetchData(FILE*, char* [], char* [], int);
 void closeFile(FILE*);
 
-/*========================================
- *              FILE OPS
-  ========================================*/
-
-// ENCRYPTION UTILS
-void enc_writeBufferData(char* file_outDest, uint8_t* src_outBuffer, uint64_t size);
-void fileops_decryptBuffer(struct AES_ctx ctx, uint8_t* encryptedBuffer, uint8_t* key, uint8_t* iv, size_t bufferSize, char* srcBuffer);
-
+// FILEOPS
 void file_fetch_Datasheetctx(FILE*);
-
-// RENDER SYSTEM OPERATIONS
 Renderctx render_init_ctx(DATASHEET sessionSheet);
 
+// SAFEGUARD
+
+// RENDER SYSTEM OPERATIONS
+
 // DATASHEET OPERATIONS
-void fetch_gradeTable(DATASHEET Datactx, int distributionTable[11][2]);
-void fetch_studentDistTable(DATASHEET Datactx);
 
-void datasheet_init_demo();
-void appendIndex(INDEX);
-void delIndex(INDEX);
-
-DATASHEET initSheetDemo();
-
-/*========================================
-                    SORT
-  ========================================*/
+// SORT MODULES
 SUBSHEET rankerModule();
 SUBSHEET masterlistModule();
 #endif
