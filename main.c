4014e073c7fc02b80a6bd76699ba9adfd8525a9a
Toff <christopherjr.abadillo@bicol-u.edu.ph>
2022-05-22 21:33:53 +0800

    Removed legacy docs

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
       MAIN EXECUTABLE
       Author: Christopher Abadillos Jr.
   ========================================*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -20,44 +19,14 @@
 // Navigation System
 #include "include/render/navigation/navigation.h"
 
-/*
-
-    PROCESS TREE
-
-        1. Open file Through IO.h | Decrypt with corresponding passkey
-        2. Disassemble Sheet for Ranker & Masterlist Modules
-        3. Send Subsheet to Modules (Ranker & Masterlist)
-        4. Get Sorted Subsheets
-        5. Send Sorted Subsheets to Render.h
-        6. Render Graph, Table, and Masterlist
-        7. Open navigation buffer
-
-
-    ACCESS TREE
-
-        - Only [Navigation.c/h] & [Main.c] has access to [Render.c/h]
-            - Only Main.c & Navigation.c/h will be rendering frames / causing frame re-renders
-
-        - Only [Navigation.c/h] & [Main.c] has access to [IO.h]
-            - Only Main.c & Navigation.c/h will be using FileOps
-
-        - Only [Main.c] has access to [/proc] modules
-
-        - Only [IO.h] has access to [Decrypt.c], [Encrypt.c], & [CRUD.c]
-            - IO.h serves as a collective interface to multiple CRUD / FileOps modules
-
-*/
-
 #define WINDOWTITLE "TESCORE: Grading Sheet Inspector"
 #define COMMANDLOGMAXENTRY 10
 
 // DEVOPS
-#define SANDBOX 1
+#define DEBUGMODE 0
+#define SANDBOX 0
 #define SANDBOXID 1
 void sandbox_scriptTesting();
-
-// GLOBALS
-FILE*       SessionSheetFile;
 
 //#region CONSOLE WINDOW RESIZE FUNCTION
 HWND WINAPI GetConsoleWindowNT(void)
@@ -94,19 +63,22 @@ int main() {
 
     // Initialize Default Render Context
     global_Sheetctx = save_readsheetctx();
+    sprintf(commandLog[0], "Finished Loading Data From: %s | Press [H] to show controls / keybindings", global_Sheetctx.address);
 
     // Deconstruct sheet
     global_renderctx = init_renderctx(global_Sheetctx);
-    printf("received address: %s\n", global_Sheetctx.address);
-    system("pause");
-    // CHECKS RENDER SYSTEM MODE (TESTING / RELEASE)
-//    SANDBOX == 1 ? CRUD_TEST(global_Sheetctx): 0;
 
-    // START NAVIGATION HANDLER AFTER INIT RENDER
-    refreshFrame(&global_renderctx, &global_Sheetctx, commandLog);
+    DEBUGMODE == 1 ? printf("received address: %s\n", global_Sheetctx.address): 0;
+    DEBUGMODE == 1 ? system("pause"): 0;
+
+    // Check for testing / release mode
+    SANDBOX == 1 ? CRUD_TEST(global_Sheetctx): 0;
+
+    // Refresh frame & start handler
+    refreshFrame(&global_renderctx, &global_Sheetctx, commandLog, 0);
     navigationKeyHandler(&global_renderctx,  &global_Sheetctx, commandLog);
 
-    // PROGRAM INTERRUPTION
+
     puts("PROGRAM INTERRUPTED");
     system("pause");
     return EXIT_SUCCESS;
@@ -116,7 +88,7 @@ void sandbox_scriptTesting(){
 
     //#region =========== SANDBOX SCRIPTS ===========
     if(SANDBOXID == 1){
-//        script_fileopsTesting();
+
     } else
     if(SANDBOXID == 2){
         exit(0);
